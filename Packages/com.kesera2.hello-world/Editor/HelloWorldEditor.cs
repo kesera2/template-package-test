352d161ed2cd5a391ab15c5169286c32b87cded2
kesera2 <38027941+kesera2@users.noreply.github.com>
2025-02-13 02:38:07 +0900

    Update .gitignore

---
+++ Packages/com.kesera2.hello-world/Editor/HelloWorldEditor.cs
@@ -0,0 +1,19 @@
+﻿using UnityEditor;
+using UnityEngine;
+
+public class HelloWorldEditor : EditorWindow
+{
+    [MenuItem("Window/Hello World Editor")]
+    public static void ShowWindow()
+    {
+        GetWindow<HelloWorldEditor>("Hello World");
+    }
+
+    private void OnGUI()
+    {
+        if (GUILayout.Button("Click Me"))
+        {
+            Debug.Log("Hello, World!");
+        }
+    }
+}
